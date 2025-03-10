4c84dbe16bb82dfb00764371f945173738acfe41
om prakash <38375675+omprakash-sit@users.noreply.github.com>
2025-03-10 22:20:01 +0530

    docker compose file added, issue with mysql connection

--- my-shop-nodejs/connection.js
+++ my-shop-nodejs/connection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 'mysql',
+    user: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'myshop'
     // insecureAuth: false
 });
 connection.connect((err) => {
