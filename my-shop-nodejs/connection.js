36ca2a11f7460380e2c20e371cf4d61c10c7ca43
om prakash <38375675+omprakash-sit@users.noreply.github.com>
2025-09-09 12:41:18 +0530

    mysql connection pool issue with individual run:fixed

--- my-shop-nodejs/connection.js
+++ my-shop-nodejs/connection.js
@@ -1,21 +1,61 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 'mysql',
+// var connection = mysql.createConnection({
+//     host: process.env.DB_HOST || 'mysql',  // 'localhost', //'127.0.0.1',
+//     port: process.env.DB_PORT || '3306',
+//     user: process.env.DB_USERNAME || 'root',
+//     password: process.env.DB_PASSWORD || 'root',
+//     database: process.env.DB_NAME || 'myshop'
+//     // insecureAuth: false
+// });
+// connection.connect((err) => {
+//     if (!err) {
+//         console.log("mysql connected on %s@%s", process.env.DB_HOST, process.env.DB_PORT);
+//     } else {
+//         console.log('%c mysql connection error', 'color: red;');
+//         console.error(err);
+//     }
+// });
+
+// // function connectWithRetry(retries = 10, delay = 2000) {
+// //     connection.connect((err) => {
+// //         if (!err) {
+// //             console.log("✅ MySQL connected on %s@%s", process.env.DB_HOST, process.env.DB_PORT);
+// //         } else {
+// //             console.error("❌ MySQL connection failed:", err.message);
+// //             if (retries > 0) {
+// //                 console.log(`🔁 Retrying in ${delay}ms... (${retries} left)`);
+// //                 setTimeout(() => connectWithRetry(retries - 1, delay), delay);
+// //             } else {
+// //                 console.error("🚫 All retries failed. Exiting.");
+// //                 process.exit(1);
+// //             }
+// //         }
+// //     });
+// // }
+
+// // connectWithRetry();
+
+// module.exports = connection;
+
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || '3306',
     user: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'myshop'
-    // insecureAuth: false
 });
-connection.connect((err) => {
-    if (!err) {
-        console.log("mysql connected on %s@%s", process.env.DB_HOST, process.env.DB_PORT);
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error("❌ MySQL connection failed:", err.message);
     } else {
-        console.log('%c mysql connection error', 'color: red;');
-        console.error(err);
+        console.log("✅ MySQL connected using pool.");
+        connection.release(); // release back to pool
     }
 });
 
-module.exports = connection;
+module.exports = pool;
--- my-shop-ui/README.md
+++ my-shop-ui/README.md
@@ -29,3 +29,6 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
 
+## docker 
+To run docker command run > docker-compose up --build
+To stop all container run > docker-compose down -v
